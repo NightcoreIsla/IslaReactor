bb9debe7f5e6ff26b28e91b9b475dd4f4f7cf946
IslaLiu <lipaming8000@gmail.com>
2023-11-04 08:04:47 +0800

    feat: -Add Try Catch prevent Reactor breaking/rebuild & Restart after Reconnect

--- main.ts
+++ main.ts
@@ -6,33 +6,46 @@ function GetReactor(): FissionReactorPeripheral {
 }
 let reactor = GetReactor();
 while (true) {
-    if (reactor == null) {
-        print("No Reactor found...Keep looking...");
-        while (reactor != null) {
-            reactor = GetReactor();
+    try {
+        let coolPercent = reactor.getCoolantFilledPercentage() * 100;
+        let isRunning = reactor.getStatus();
+        let tempc = reactor.getTemperature() - 273.15;
+        let waste = reactor.getWasteFilledPercentage() * 100;
+        let damage = reactor.getDamagePercent();
+        let heatcoolPercent = reactor.getHeatedCoolantFilledPercentage() * 100;
+
+
+        print("cool:" + coolPercent);
+        print("Running:" + isRunning);
+        print("tempc:" + tempc);
+        print("waste:" + waste);
+        print("damage:" + damage);
+        print("heatcool:" + heatcoolPercent);
+        if (isRunning && (coolPercent < 50 || tempc >= 150 ||  waste > 80 || damage >= 5 || heatcoolPercent >= 50)) {
+            reactor.scram();
+            print("Stopped");
         }
-    }
-    let coolPercent = reactor.getCoolantFilledPercentage() * 100;
-    let isRunning = reactor.getStatus();
-    let tempc = reactor.getTemperature() - 273.15;
-    let waste = reactor.getWasteFilledPercentage() * 100;
-    let damage = reactor.getDamagePercent();
-    let heatcoolPercent = reactor.getHeatedCoolantFilledPercentage() * 100;
-
-
-    print("cool:" + coolPercent);
-    print("Running:" + isRunning);
-    print("tempc:" + tempc);
-    print("waste:" + waste);
-    print("damage:" + damage);
-    print("heatcool:" + heatcoolPercent);
-    if (isRunning && (coolPercent < 50 || tempc >= 150 ||  waste > 80 || damage >= 5 || heatcoolPercent >= 50)) {
-        reactor.scram();
-        print("Stopped");
-    }
-    else if (!isRunning && (coolPercent == 100 && tempc <= 130 && waste == 0 && damage == 0 && heatcoolPercent < 50)) {
-        reactor.activate();
-        print("Started");
+        else if (!isRunning && (coolPercent == 100 && tempc <= 130 && waste == 0 && damage == 0 && heatcoolPercent < 50)) {
+            reactor.activate();
+            print("Started");
+        }
+    } catch (error) {
+        let tried = 0;
+        let success = false
+        while (!success) {
+            success = true;
+            try {
+                reactor = GetReactor();
+                reactor.getTemperature();
+            } catch (error) {
+                success = false;
+                print("error:" + error);
+                print("Error Or No Reactor found...Keep looking...");
+                print(tried);
+                tried += 1;
+            }
+            os.sleep(1);
+        }
     }
     os.sleep(1);
 }
