c1c5a6f421fa0bbb37820838f1f2603c1049bcac
IslaLiu <lipaming8000@gmail.com>
2023-11-04 18:45:56 +0800

    feat:[gui] add a empty gui with basalt it still need refactor type library for add element

--- main.ts
+++ main.ts
@@ -1,9 +1,33 @@
 import * as event from "./event";
+import * as Basalt from "./types/basalt"
+
 
 function GetReactor(): FissionReactorPeripheral {
     let [device] = peripheral.find("fissionReactorLogicAdapter");
     return <FissionReactorPeripheral>device;
 }
+
+const basalt = <Basalt.Basalt>require('basalt.lua');
+
+const frame = basalt.createFrame();
+
+/*frame.addText(1, 1,"Coolant:");
+frame.addText(1, 2,"Status:");
+frame.addText(1, 3, "Temperature:");
+frame.addText(1, 4, "Waste:");
+frame.addText(1, 5, "Damage:");
+frame.addText(1, 6, "Heated Coolant:");
+
+const coolText = frame.addText(2, 1,"0%");
+const statusText = frame.addText(2, 2,"Unknow");
+const tempText = frame.addText(2, 3, "0C");
+const wasteText = frame.addText(2, 4, "0%");
+const damageText = frame.addText(2, 5, "0%");
+const heatcoolText = frame.addText(2, 6, "0%");*/
+
+basalt.autoUpdate();
+
+
 let reactor = GetReactor();
 while (true) {
     try {
@@ -14,13 +38,6 @@ while (true) {
         let damage = reactor.getDamagePercent();
         let heatcoolPercent = reactor.getHeatedCoolantFilledPercentage() * 100;
 
-
-        print("cool:" + coolPercent);
-        print("Running:" + isRunning);
-        print("tempc:" + tempc);
-        print("waste:" + waste);
-        print("damage:" + damage);
-        print("heatcool:" + heatcoolPercent);
         if (isRunning && (coolPercent < 50 || tempc >= 150 ||  waste > 80 || damage >= 5 || heatcoolPercent >= 50)) {
             reactor.scram();
             print("Stopped");
